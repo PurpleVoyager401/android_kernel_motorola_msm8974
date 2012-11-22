b905fc79972e3f98762e2439755c371b0b5d1652
Rusty Russell <rusty@rustcorp.com.au>
2012-11-22 12:30:25 +1030

    __UNIQUE_ID()
    Jan Beulich points out __COUNTER__ (gcc 4.3 and above), so let's use
    that to create unique ids.  This is better than __LINE__ which we use
    today, so provide a wrapper.
    
    Stanislaw Gruszka <sgruszka@redhat.com> reported that some module parameters
    start with a digit, so we need to prepend when we for the unique id.
    
    Change-Id: I9fc3ae5af29419bcccd14e80fdc6dffafd2860c3
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -42,6 +42,10 @@ extern void __chk_io_ptr(const volatile void __iomem *);
 # define __rcu
 #endif
 
+/* Indirect macros required for expanded argument pasting, eg. __LINE__. */
+#define ___PASTE(a,b) a##b
+#define __PASTE(a,b) ___PASTE(a,b)
+
 #ifdef __KERNEL__
 
 #ifdef __GNUC__
@@ -164,9 +168,9 @@ void ftrace_likely_update(struct ftrace_branch_data *f, int val, int expect);
     (typeof(ptr)) (__ptr + (off)); })
 #endif
 
-
-#ifndef OPTIMIZER_HIDE_VAR
-#define OPTIMIZER_HIDE_VAR(var) barrier()
+/* Not-quite-unique ID. */
+#ifndef __UNIQUE_ID
+# define __UNIQUE_ID(prefix) __PASTE(__PASTE(__UNIQUE_ID_, prefix), __LINE__)
 #endif
 
 #include <linux/types.h>
